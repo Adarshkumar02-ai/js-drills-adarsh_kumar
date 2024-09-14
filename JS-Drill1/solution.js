a327e1b4b589a596ce46f4e86536ac667a68a9ea
Adarshkumar02-ai <anishrj715@gmail.com>
2024-09-14 15:15:03 +0530

    Fetched the emails of all the individuals from the given data

--- JS-Drill1/solution.js
+++ JS-Drill1/solution.js
@@ -1,1 +1,14 @@
-import arrayOfObjects from "./persons";
+import arrayOfObjects from "./persons.js";
+
+
+
+// Given the dataset of individuals, write a function that accesses and returns the email addresses of all individuals.
+
+function getEmails(){
+    let emails=[];
+    for(let i=0;i<arrayOfObjects.length;i++){
+      emails.push(arrayOfObjects[i].email);
+    }
+     return emails;
+  }
+  console.log(getEmails());
