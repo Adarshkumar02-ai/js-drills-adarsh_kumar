239c9198c9710c09033677a544793a737cd33688
Adarshkumar02-ai <anishrj715@gmail.com>
2024-09-14 16:15:24 +0530

    fetched the users whose stay in Germany

--- JS-Drill2/solution.js
+++ JS-Drill2/solution.js
@@ -13,3 +13,18 @@ function intrestedInVideogames(){
     return result;
 }
 console.log(intrestedInVideogames());
+
+
+
+// Q2 Find all users staying in Germany.
+
+function findUsersInGermany() {
+    const result = [];
+    for (const i in users) {
+        if (users[i].nationality === "Germany") {
+                  result.push(i);
+        }
+    }
+    return result;
+}
+console.log(findUsersInGermany());
