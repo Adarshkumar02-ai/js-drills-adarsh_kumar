bed3c5e4882bf1a36ef1df63dca89093ec57dd7b
Adarshkumar02-ai <anishrj715@gmail.com>
2024-09-14 16:13:30 +0530

    fetched the users who are intrested in playing video games

--- JS-Drill2/solution.js
+++ JS-Drill2/solution.js
@@ -1,1 +1,15 @@
-import users from "./users";
+import users from "./users.js";
+
+// Q1 Find all users who are interested in playing video games.
+
+function intrestedInVideogames(){
+    const result = [];
+    for(const i in users){
+        if(users[i].interests.includes( "Video Games")){
+            result.push(i);
+            break;
+        }
+    }
+    return result;
+}
+console.log(intrestedInVideogames());
