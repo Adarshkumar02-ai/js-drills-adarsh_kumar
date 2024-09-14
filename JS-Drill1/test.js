efb6f1aadf3621ef11e3586bf5105abff7b8b73b
Adarshkumar02-ai <anishrj715@gmail.com>
2024-09-14 14:56:54 +0530

    Added person.js, solution.js and test.js

---
+++ JS-Drill1/test.js
@@ -0,0 +1,20 @@
+import {
+    getEmails,
+       getHobbies,
+       getStudents,
+       nameCity,
+       firstHobby,
+       ages,
+       namesAndEmail,
+       cityAndCountry
+} from './solution.js'
+
+
+console.log(getEmails());
+console.log(getHobbies(30));
+console.log(getStudents());
+nameCity(3);
+ages();
+console.log(firstHobby());
+namesAndEmail(25);
+cityAndCountry();
