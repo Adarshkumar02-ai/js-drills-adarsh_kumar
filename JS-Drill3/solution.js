eb2bf4bc8e33cd3e1e6181992872a7bd75843ef6
Adarshkumar02-ai <anishrj715@gmail.com>
2024-09-16 10:48:58 +0530

    fetched the data of only BMW and Audi cars from the given data

--- JS-Drill3/solution.js
+++ JS-Drill3/solution.js
@@ -96,3 +96,21 @@ function countCarsOlderThan2000() {
     // console.log(olderCars);
   }
   countCarsOlderThan2000();
+
+
+
+//    ==== Problem #6 ====
+// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Execute a function and return an array that only contains BMW and Audi cars.  Once you have the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+
+
+
+function getBMWandAudi(){
+    const result = [];
+    for(let i=0; i<inventory.length;i++){
+        if(inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi"){
+            result.push(inventory[i]);
+        }
+    }
+    console.log(JSON.stringify(result));
+}
+getBMWandAudi();
