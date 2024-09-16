f9fa88da660e0ef4ba204434921b15cec6129103
Adarshkumar02-ai <anishrj715@gmail.com>
2024-09-16 11:56:58 +0530

    Added the function for each

--- JS-Drill4/arrays.js
+++ JS-Drill4/arrays.js
@@ -10,13 +10,22 @@ const items = [1, 2, 3, 4, 5, 5]; // use this array to test your code.
 
 */
 
+
 function each(elements, cb) {
   // Do NOT use forEach to complete this function.
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // You should also pass the index into `cb` as the second argument
   // based off http://underscorejs.org/#each
-}
+ 
+    for (let i = 0; i < elements.length; i++) {
+      cb(elements[i], i);
+    }
+  }
+each(items, function(item, index){
+  console.log(`Item at index ${index} : ${item}`)});
+
+
 
 function map(elements, cb) {
   // Do NOT use .map, to complete this function.
