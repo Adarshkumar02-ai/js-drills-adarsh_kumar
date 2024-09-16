e88e48172c60e5573e7588c28784d4f0f43f77e9
Adarshkumar02-ai <anishrj715@gmail.com>
2024-09-16 10:37:36 +0530

    fetched the information of a car by passing Id

--- JS-Drill3/solution.js
+++ JS-Drill3/solution.js
@@ -1,1 +1,17 @@
 import inventory from "./cars.js";
+
+// / ==== Problem #1 ====
+// // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by calling a function that will return the data for that car. Then log the car's year, make, and model in the console log in the format of:
+// ("Car 33 is a *car year goes here* *car make goes here* *car model goes here*");
+
+function findCarById(id){
+    let flag 
+    for(let i=0; i<inventory.length;i++){
+        if(inventory[i].id === id){
+           flag = inventory[i];
+        }
+    }
+    console.log(`car 33 is a ${flag.car_year} ${flag.car_make} ${flag.car_model}`);
+
+}
+findCarById(33);
