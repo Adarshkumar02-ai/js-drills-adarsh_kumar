9c90273f20133d0538c212929502ea78ade7345f
Adarshkumar02-ai <anishrj715@gmail.com>
2024-09-17 11:10:13 +0530

    Added the function to the counterFactory

--- JS-Drill6/closures.js
+++ JS-Drill6/closures.js
@@ -3,14 +3,34 @@ function counterFactory() {
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 
-  
+  let counter = 0; // Private variable
+
+  return {
+    increment() {
+      counter += 1;
+      return counter;
+    },
+    decrement() {
+      counter -= 1;
+      return counter;
+    }
+  };
 }
+
+const counter = counterFactory();
+console.log(counter.increment()); 
+console.log(counter.increment()); 
+console.log(counter.decrement()); 
+
 
 function limitFunctionCallCount(cb, n) {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
   // Returning null is acceptable if cb can't be returned
+  
 }
+
+
 
 function cacheFunction(cb) {
   // Should return a function that invokes `cb`.
@@ -19,4 +39,5 @@ function cacheFunction(cb) {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+ 
 }
